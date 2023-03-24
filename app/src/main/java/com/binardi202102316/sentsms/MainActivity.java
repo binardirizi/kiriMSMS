0ad38f61a60c418597c3d400830d07f75b7aedec
Aurora ivana sk <ivanaaurora064@gmail.com>
2023-03-24 17:24:59 +0700

    Tugas 4

--- app/src/main/java/com/binardi202102316/sentsms/MainActivity.java
+++ app/src/main/java/com/binardi202102316/sentsms/MainActivity.java
@@ -3,12 +3,46 @@ package com.binardi202102316.sentsms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button sendBtn;
+
+    EditText txtphoneNo;
+
+    EditText txtMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendBtn = (Button) findViewById(R.id.btn_kirim);
+        txtphoneNo = (EditText) findViewById(R.id.ed_no);
+        txtMessage = (EditText) findViewById(R.id.ed_pesan);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
+    }
+    protected void sendSMSMessage(){
+        Log.i("Send SMS", "");
+        String phoneNo = txtphoneNo.getText().toString();
+        String message = txtMessage.getText().toString();
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(),"SMS Sent.", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS Failed, Please Try Again.", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
